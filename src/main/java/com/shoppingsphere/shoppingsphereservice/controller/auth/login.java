2bf7030ffb2296b3262c2b89b05eb2abafd03043
haihaycode <kienthuc177@gmail.com>
2024-05-23 23:41:47 +0700

    chán

---
+++ src/main/java/com/shoppingsphere/shoppingsphereservice/controller/auth/login.java
@@ -0,0 +1,15 @@
+package com.shoppingsphere.shoppingsphereservice.controller.auth;
+
+import com.shoppingsphere.shoppingsphereservice.navigation.BreadcrumbPath;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class login {
+    @GetMapping("/login")
+    public String register(Model model){
+        model.addAttribute("breadcrumbs", BreadcrumbPath.LOGIN.getBreadcrumbs());
+        return "auth/login";
+    }
+}
--- src/main/java/com/shoppingsphere/shoppingsphereservice/controller/client/homeController.java
+++ src/main/java/com/shoppingsphere/shoppingsphereservice/controller/client/homeController.java
@@ -1,13 +1,16 @@
 package com.shoppingsphere.shoppingsphereservice.controller.client;
 
+import com.shoppingsphere.shoppingsphereservice.navigation.BreadcrumbPath;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class homeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("breadcrumbs", BreadcrumbPath.HOME.getBreadcrumbs());
         return "index";
     }
 }
