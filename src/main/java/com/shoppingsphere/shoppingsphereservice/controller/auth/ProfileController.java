7225e74b61aaf5567a7d3cd11497f627e352aecd
haihaycode <kienthuc177@gmail.com>
2024-05-26 20:40:50 +0700

    giao diện phần auth -)))

---
+++ src/main/java/com/shoppingsphere/shoppingsphereservice/controller/auth/ProfileController.java
@@ -0,0 +1,34 @@
+package com.shoppingsphere.shoppingsphereservice.controller.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class ProfileController {
+    @GetMapping("")
+    public String ManageMyAccount(){
+        return "client/account/ManageMyAccount";
+    }
+    @GetMapping("/MyProfile")
+    public String MyProfile(){
+        return "client/account/myProfile";
+    }
+    @GetMapping("/AddressBook")
+    public String AddressBook(){
+        return "client/account/AddressBook";
+    }
+    @GetMapping("/TrackYourOrder")
+    public String TrackYourOrder(){
+        return "client/account/TrackOrder";
+    }
+    @GetMapping("/myOrders")
+    public String MyOrders(){
+        return "client/account/MyOrders";
+    }
+    @GetMapping("/MyReturnsAndCancellations")
+    public String MyReturnsAndCancellations(){
+        return "client/account/MyReturnsAndCancellations";
+    }
+}
